793f26f9cc84892c5fed235e5130dc0603c6a01e
schao523 <schao523@gmail.com>
2026-04-24 12:22:34 +0800

    Export vector store factory helpers

--- rag_subsystem/__init__.py
+++ rag_subsystem/__init__.py
@@ -2,6 +2,7 @@
 from .config import ProcessConfig, RetrievalConfig, DEFAULT_PROCESS_CONFIG, DEFAULT_RETRIEVAL_CONFIG
 from .process_files import process_files
 from .retrieval_data import retrieve_data
+from .vector_store.factory import create_vector_store, get_default_vector_store
 __all__ = [
     "ProcessConfig",
     "RetrievalConfig",
@@ -9,4 +10,6 @@ __all__ = [
     "DEFAULT_RETRIEVAL_CONFIG",
     "process_files",
     "retrieve_data",
+    "create_vector_store",
+    "get_default_vector_store",
 ]
